3272e63c386f4431f5ac3fcc9bcfbf62f8bfff07
Xrave <blueboy12ye@gmail.com>
2026-07-15 20:54:05 +0700

    Fix: The log was not working, causing the module to fail

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,5 @@
 #!/system/bin/sh
 
-rm -f /data/local/tmp/module
-
 SERVICES='
   mi_thermald
   thermal
@@ -11,5 +9,5 @@ SERVICES='
 
 for svc in $SERVICES; do
   service call miui.mqsas.IMQSNative 21 i32 1 s16 'setprop' i32 1 \
-    s16 "ctl.start $svc" s16 "storage/emulated/0/Berangkas/log/log_off.txt" i32 1 > /dev/null
+    s16 "ctl.start $svc" s16 "storage/emulated/0/log.txt" i32 1 > /dev/null
 done
